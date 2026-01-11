a560b425c1d0335ac43296a64e1a4cb744ad1554
JJ <jadeandpearls@gmail.com>
2026-01-10 19:47:36 -0700

    image transform; env; remove node modules;

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
 const yaml = require("js-yaml");
+require('dotenv').config();
+
+const pluginImages = require("./eleventy.config.images.js");
+
+const isDev = process.env.NODE_ENV === 'development';
+const baseUrl = isDev ? `localhost:8081` : `https://www.joy-jade.com`;
+
+const toAbsoluteUrl = (url) => {
+  return new URL(url, baseUrl).href;
+}
 
 module.exports = function(eleventyConfig) {
 
@@ -11,12 +21,40 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
+  eleventyConfig.addFilter("linkungan", (x) => {
+    if (process.env.NODE_ENV === 'development') {
+      return `linkungan: dev`
+    } else {
+      return 'not dev'
+    }
+  });
+
+  eleventyConfig.addFilter('toAbsoluteUrl', toAbsoluteUrl);
+  
+  //App plugins
+  eleventyConfig.addPlugin(pluginImages);
+
   // config to choose where you want your source files and public files
   return {
+    // When a passthrough file is modified, rebuild the pages:
+    passthroughFileCopy: true,
+
+    templateFormats: [
+			"md",
+			"njk",
+			"html"
+		],
+
+    htmlTemplateEngine: "njk",
+
     dir: {
       input: "src",
       output: "_site",
-      data:'_data'
-    }
+      data:'_data',
+      output: "_site",
+    },
+
+    pathPrefix: "/",
+
   };
 }
